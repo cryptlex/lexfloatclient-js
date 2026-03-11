7456fcaa8a838563557a49137791a58139a44048
Huzaif Mushtaq Mir <huzaif@Huzaif-MacBook-Air.local>
2026-03-11 17:54:09 +0530

    fix: correct notice file names

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -19,5 +19,5 @@ cp ./build/Release/lexfloatclient.node ./lib/bindings/macos/x64
 
 SDKROOT=$(xcrun --sdk macosx --show-sdk-path) node-gyp rebuild --arch=arm64
 cp ./build/Release/lexfloatclient.node ./lib/bindings/macos/arm64
-cp ./tmp/macos/THIRD_PARTY_NOTICES.txt ./lib/bindings/macos/
+cp ./tmp/macos/THIRD-PARTY-NOTICES.txt ./lib/bindings/macos/
 rm -f LexFloatClient-Static-Mac.zip
